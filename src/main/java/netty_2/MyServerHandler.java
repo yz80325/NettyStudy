109222e27a7eaa15c4af38bef23ff09e812b84b7
yz2021 <yz202113@163.com>
2020-11-09 22:42:34 +0900

    基础学习

---
+++ src/main/java/netty_2/MyServerHandler.java
@@ -0,0 +1,20 @@
+package netty_2;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandler;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+import java.nio.charset.Charset;
+import java.util.Date;
+
+public class MyServerHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        ByteBuf buf=(ByteBuf) msg;
+        byte[]myBytes=new byte[buf.readableBytes()];
+        buf.readBytes(myBytes);
+        System.out.print(new Date() + "接收到消息：");
+        System.out.println(new String(myBytes, Charset.forName("GBK")));
+    }
+}
